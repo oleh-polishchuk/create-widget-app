94bb9d99c96d4d43d561922d6cb648517773dc0a
Oleh Polishchuk <oleh.polishchuk.ck@gmail.com>
2020-11-25 11:35:17 +0200

    add reset css

--- templates/index-html.template.js
+++ templates/index-html.template.js
@@ -5,41 +5,53 @@ module.exports = Object.assign({
     template: ({ className }) => `\
 <html>
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Elementor Widget</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Elementor Widget</title>
+    <style>
+        * {
+            box-sizing: border-box;
+        }
+
+        html, body {
+            margin: 0;
+            padding: 0;
+        }
+    </style>
 </head>
 <body>
 
-  <style>
-    .${className} {}
-  </style>
-  
-  <div class="${className}" data-selector="${className}">
-    Widget works!
-  </div>
+    <style>
+        .${className} {}
+    </style>
 
-  <script>
-    init();
+    <div class="${className}" data-selector="${className}">
+        Widget works!
+    </div>
 
-    ////
+    <script>
+        init();
 
-    function init() {
-      log('Start initialization...')
-      main();
-    }
+        ////
 
-    function main() {
-      log('Initialization success!')
-      handlerA();
-    }
-    
-    function handlerA() {
-         // your code here...
-    }
-    
-    function log(...message) { console.log('[${className}]', ...message); }
-  </script>
+        function init() {
+            log('Start initialization...')
+            main();
+        }
+
+        function main() {
+            log('Initialization success!')
+            handlerA();
+        }
+
+        function handlerA() {
+            // your code here...
+        }
+
+        function log(...message) {
+            console.log('[${className}]', ...message);
+        }
+    </script>
 
 </body>
 </html>
